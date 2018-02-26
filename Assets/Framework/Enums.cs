1dbba520425b32864f7f66f4449288bf82cfb82a
Tyler Ahlstrom <32003791+tmahlstrom@users.noreply.github.com>
2018-02-25 20:55:37 -0500

    temp clear out

--- Assets/Framework/Enums.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RTS {
-	public enum CursorState {DefaultCursor, AllyCursor, EnemyCursor, TransparentCursor}
-	public enum FlySpawnType {Black, Red, Yellow}
-	public enum HealSpawnType {Level_1, Level_2}
-	public enum AntSpawnType {Red}
-	public enum ItemType {OrganicMatter, Corpse} 
-    public enum EAnimation {Null, Idle, Move, Rotate, Spawn, Attack, SpawnerSpecial, Build, Revive, Die, Sit}
-}
--- Assets/Player/TeamColor.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine;
-
-public class TeamColor : MonoBehaviour {
-	//wrapper used to determine what to apply the team color to
-}
