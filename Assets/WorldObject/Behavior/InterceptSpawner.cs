ae7213eade317f9f6f9b902dc81b809f9034921c
tmahlstrom <32003791+tmahlstrom@users.noreply.github.com>
2018-04-16 12:00:16 -0400

    formatting

--- Assets/WorldObject/Behavior/InterceptSpawner.cs
+++ Assets/WorldObject/Behavior/InterceptSpawner.cs
@@ -18,7 +18,7 @@ public class InterceptSpawner : Spawner {
 	}
 
 	protected override void Update () {
-        base.Update();
+        	base.Update();
 	}
 
 
@@ -27,7 +27,7 @@ public class InterceptSpawner : Spawner {
 	protected virtual IEnumerator PeriodicallyConsiderIncreasingMana (){
 		while (this != null) {
 			//if (worldObject.GetEnemyTargetsInRange (15).Count >= 1) {
-                unit.ChangeMana(1);
+                		unit.ChangeMana(1);
 				yield return new WaitForSeconds (0.8f);
 			// } else {
 			// 	yield return new WaitForSeconds (3.0f); 
@@ -36,9 +36,9 @@ public class InterceptSpawner : Spawner {
 	}
 
 	protected override void CreateSpawn () {
-        base.CreateSpawn(); 
-        for (int i = 0; i < paramManager.SpawnPerCreate; i++) {
-            GameObject spawnGO = (GameObject)Instantiate (paramManager.SpawnablesList[0], DetermineSpawnPoint (), Quaternion.Euler (-90, 0, 0));
+        	base.CreateSpawn(); 
+        	for (int i = 0; i < paramManager.SpawnPerCreate; i++) {
+           		GameObject spawnGO = (GameObject)Instantiate (paramManager.SpawnablesList[0], DetermineSpawnPoint (), Quaternion.Euler (-90, 0, 0));
 			spawnGO.transform.SetParent (spawnHolder.transform);
 			spawnGO.SetActive (true);
 			Spawn spawn = spawnGO.GetComponent< Spawn > ();
@@ -47,34 +47,34 @@ public class InterceptSpawner : Spawner {
 	}
 
 	protected override Vector3 DetermineSpawnPoint(){
-        spawnGenerationPoint = transform.position;
-        Orb orb = transform.parent.GetComponentInChildren<Orb>();
-        if (orb){
-            spawnGenerationPoint = orb.transform.position; 
-        }
+        	spawnGenerationPoint = transform.position;
+        	Orb orb = transform.parent.GetComponentInChildren<Orb>();
+        	if (orb){
+            		spawnGenerationPoint = orb.transform.position; 
+        	}
 		return spawnGenerationPoint;
 	}
 
 	protected override void CreateSpawnsTransform (){
 		spawnHolder = new GameObject();
-        spawnHolder.name = worldObject.name + " Spawns";
+        	spawnHolder.name = worldObject.name + " Spawns";
 		//SpawnHolder.transform.position = DetermineSpawnPoint (); 
-        Orb orb = transform.parent.GetComponentInChildren<Orb>();
-        if (orb){
-            spawnHolder.transform.SetParent (orb.gameObject.transform); 
-        }
+        	Orb orb = transform.parent.GetComponentInChildren<Orb>();
+        	if (orb){
+            		spawnHolder.transform.SetParent (orb.gameObject.transform); 
+        	}
 	}
 
 	public override bool ReadyToSpawnerSpecial(){
 		if (unit.paramManager.ManaPoints >= (unit.paramManager.MaxManaPoints/10)){
 			return true; 
 		}
-        return false; 
+        	return false; 
 	}
 
 	public override void InitiateSpawnerSpecial(){ 
 		specialProcessActive = true; 
-        StartCoroutine("UltimateIntercept");
+        	StartCoroutine("UltimateIntercept");
 		SpecialSound(true); 
 	}
 
@@ -85,13 +85,13 @@ public class InterceptSpawner : Spawner {
 
 	public IEnumerator UltimateIntercept(){
 		InterceptSpawn interceptSpawn = GetComponentInChildren<InterceptSpawn> ();
-        //interceptSpawn.laserCooldown = paramManager.SpawnerSpecialEffectRate;
+        	//interceptSpawn.laserCooldown = paramManager.SpawnerSpecialEffectRate;
 		interceptSpawn.laserEnabled = true; 
-        while (unit.paramManager.ManaPoints > 0.0f && specialProcessActive) {
+        	while (unit.paramManager.ManaPoints > 0.0f && specialProcessActive) {
 			unit.ChangeMana (-1);
-            yield return new WaitForSeconds (paramManager.SpawnerSpecialDurationFactor);
+            		yield return new WaitForSeconds (paramManager.SpawnerSpecialDurationFactor);
 		}
-        unit.unitState.SelfExitState(RTS.EAnimation.SpawnerSpecial);
+        	unit.unitState.SelfExitState(RTS.EAnimation.SpawnerSpecial);
 		// interceptSpawn.laserCooldown = paramManager.SpawnCooldown;
 		interceptSpawn.laserEnabled = false; 
     }
