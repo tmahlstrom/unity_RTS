761a11177edea04c80a15edff1407aadaf81beeb
tmahlstrom <32003791+tmahlstrom@users.noreply.github.com>
2018-04-16 12:17:24 -0400

    formatting

--- Assets/WorldObject/StateScripts/UnitStates/MoveState.cs
+++ Assets/WorldObject/StateScripts/UnitStates/MoveState.cs
@@ -16,11 +16,11 @@ public class MoveState : UnitBaseState {
 
 	public override void SelfExitState(RTS.EAnimation state){
 		if (state == RTS.EAnimation.Move){
-            manuallyInitatedState = false;
-            unit.animationManager.ChangeAnimation(RTS.EAnimation.Move, false);
-            manuallyInitatedState = false;
-            //unit.mover.ClearMovement(); this causes problem of turning to look at previous move point after arrival at basic ally interact
-            unit.SetUnitState(new IdleState(unit, false));
+		    manuallyInitatedState = false;
+		    unit.animationManager.ChangeAnimation(RTS.EAnimation.Move, false);
+		    manuallyInitatedState = false;
+		    //unit.mover.ClearMovement(); this causes problem of turning to look at previous move point after arrival at basic ally interact
+		    unit.SetUnitState(new IdleState(unit, false));
 		}
 	}
 }
