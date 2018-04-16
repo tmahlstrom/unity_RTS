1918212f8b51749a7ad4e9d1e80fb381b3786533
tmahlstrom <32003791+tmahlstrom@users.noreply.github.com>
2018-04-16 12:19:11 -0400

    formatting

--- Assets/WorldObject/StateScripts/UnitStates/ReviveState.cs
+++ Assets/WorldObject/StateScripts/UnitStates/ReviveState.cs
@@ -20,15 +20,15 @@ public class ReviveState : UnitBaseState {
 	}
 
 	private void GetInPositionToRevive(){
-        if (unit.reviver.GOfocus){
+        	if (unit.reviver.GOfocus){
 			if (!IsEngagedWithGO(unit.reviver.GOfocus)){
 				unit.mover.PrimaryInteractGameObjectAtDistance(unit.reviver.GOfocus, 0.9f);
-                unit.animationManager.ChangeAnimation(RTS.EAnimation.Revive, false);
+                		unit.animationManager.ChangeAnimation(RTS.EAnimation.Revive, false);
 				if (reviveSound != null){
 					unit.StopCoroutine(reviveSound); 
 				}
 				reviveSound = unit.StartCoroutine(ReviveSound(false));
-                unit.animationManager.ChangeAnimation(RTS.EAnimation.Move, true);
+                		unit.animationManager.ChangeAnimation(RTS.EAnimation.Move, true);
 			} else{
 				PerformRevive();
 			}
@@ -36,8 +36,8 @@ public class ReviveState : UnitBaseState {
 	}
 
 	private void PerformRevive (){
-        unit.animationManager.ChangeAnimation(RTS.EAnimation.Move, false);
-        unit.animationManager.ChangeAnimation(RTS.EAnimation.Revive, true);
+		unit.animationManager.ChangeAnimation(RTS.EAnimation.Move, false);
+		unit.animationManager.ChangeAnimation(RTS.EAnimation.Revive, true);
 		unit.reviver.BeginRevive(); 
 		if (reviveSound != null){
 			unit.StopCoroutine(reviveSound); 
@@ -62,17 +62,17 @@ public class ReviveState : UnitBaseState {
 			}
 			GetInPositionToRevive();
 			
-            return;
-        }
+            	return;
+        	}
 		base.MouseClickRight(hitObject, hitPoint);
 	}
 
 	public override void AnimationClimaxEvent(RTS.EAnimation state){
 		if (state == RTS.EAnimation.Revive){
-            unit.reviver.ReviveExecution();
-            SelfExitState(RTS.EAnimation.Revive);
+		    unit.reviver.ReviveExecution();
+		    SelfExitState(RTS.EAnimation.Revive);
 		}
-    }
+    	}
 
 	private IEnumerator ReviveSound(bool toggle){
 		if (unit.worldObject.audioSource){
@@ -91,7 +91,7 @@ public class ReviveState : UnitBaseState {
 
 
 	public override void ExitRoutine(){
-        unit.animationManager.ChangeAnimation(RTS.EAnimation.Revive, false);
+		unit.animationManager.ChangeAnimation(RTS.EAnimation.Revive, false);
 		if (reviveSound != null){
 			unit.StopCoroutine(reviveSound); 
 		}
@@ -102,8 +102,8 @@ public class ReviveState : UnitBaseState {
 	public override void SelfExitState(RTS.EAnimation state){
 		if (state == RTS.EAnimation.Revive){
 			base.SelfExitState(state);
-            unit.mover.ClearMovement();
-            unit.SetUnitState(new IdleState(unit, false));
+			unit.mover.ClearMovement();
+			unit.SetUnitState(new IdleState(unit, false));
 		}
 	}
 
