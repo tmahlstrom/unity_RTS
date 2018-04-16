02c5cf4516c8ec0c3323357c57916e5e5f281b02
tmahlstrom <32003791+tmahlstrom@users.noreply.github.com>
2018-04-16 11:51:26 -0400

    formatting

--- Assets/WorldObject/Behavior/AntSpawner.cs
+++ Assets/WorldObject/Behavior/AntSpawner.cs
@@ -6,38 +6,38 @@ using RTS;
 public class AntSpawner : Spawner {
 
      
-    WaitForSeconds shortWait = new WaitForSeconds(0.5f);
+        WaitForSeconds shortWait = new WaitForSeconds(0.5f);
 
 
-	protected override void Awake () {
-		base.Awake (); 
-	}
+        protected override void Awake () {
+                base.Awake (); 
+        }
 
-	protected override void Start () {
-		base.Start ();
-	}
+        protected override void Start () {
+                base.Start ();
+        }
 
-	protected override void Update () {
+        protected override void Update () {
         base.Update(); 
-	}
+        }
 
 
-	protected override void CreateSpawn (){
+        protected override void CreateSpawn (){
         base.CreateSpawn(); 
         for (int i = 0; i < paramManager.SpawnPerCreate; i++) {
             CreateAntSpawn(paramManager.SpawnablesList[0]);
-		}
-	}
+                }
+        }
 
-    public virtual void CreateAntSpawn (GameObject spawnGO){
+        public virtual void CreateAntSpawn (GameObject spawnGO){
         GameObject newSpawnGameObject = Instantiate (spawnGO, DetermineSpawnPoint (), Quaternion.Euler (-90, 0, 0));
-		if (newSpawnGameObject != null) {
-			newSpawnGameObject.transform.SetParent (spawnHolder.transform);
-			newSpawnGameObject.SetActive (true);
-			Spawn spawn = newSpawnGameObject.GetComponent< Spawn > ();
-			TellSpawnWhoMadeIt (spawn);
-		}
-	}
+                if (newSpawnGameObject != null) {
+                        newSpawnGameObject.transform.SetParent (spawnHolder.transform);
+                        newSpawnGameObject.SetActive (true);
+                        Spawn spawn = newSpawnGameObject.GetComponent< Spawn > ();
+                        TellSpawnWhoMadeIt (spawn);
+                }
+        }
 
 	private void TellSpawnWhoMadeIt (Spawn spawn){
 		spawn.RememberWhoMadeMe (worldObject); 
